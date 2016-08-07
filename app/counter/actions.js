4394e1b733101db8162b49ac18548f3b8639e3b3
VIctor Alvarez <v.alvarez312@gmail.com>
2016-08-06 22:33:37 -0700

    clean

--- app/counter/actions.js
+++ app/counter/actions.js
@@ -1,16 +1,7 @@
 import { makeActionCreators } from 'redux-standard-actions'
-import { getStep } from './reducers'
 
 export const { increment, decrement, changeStep } = makeActionCreators(
   'INCREMENT',
   'DECREMENT',
   'CHANGE_STEP'
 )
-
-export function incrementThunk() {
-  return (dispatch, getState) => dispatch(increment(getStep(getState())))
-}
-
-export function decrementThunk() {
-  return (dispatch, getState) => dispatch(decrement(getStep(getState())))
-}
--- app/counter/counter.js
+++ app/counter/counter.js
@@ -2,14 +2,20 @@ import React, { PropTypes } from 'react'
 import styles from './counter.scss'
 import RaisedButton from 'material-ui/RaisedButton'
 import { connect } from 'react-redux'
-import { incrementThunk, decrementThunk } from './actions'
+import { increment, decrement } from './actions'
 import { getCount, getStep } from './reducers'
 import StepSetter from './step-setter'
 import AddCircle from 'material-ui/svg-icons/content/add-circle'
 import RemoveCircle from 'material-ui/svg-icons/content/remove-circle'
 
-const Counter = ({ count, step, onIncrement, onDecrement }) =>
-  <div>
+const Counter = ({ count, step, onIncrement, onDecrement }) => {
+  function decrementWithStep() {
+    onDecrement(step)
+  }
+  function incrementWithStep() {
+    onIncrement(step)
+  }
+  return <div>
     <p>
       A container component connected to the Redux store with synchronous dispatches and memoized selectors.
     </p>
@@ -21,18 +27,19 @@ const Counter = ({ count, step, onIncrement, onDecrement }) =>
     <RaisedButton
       className={ styles.counterButton }
       primary
-      onClick={ onIncrement }
+      onClick={ incrementWithStep }
       icon={ <AddCircle /> } />
     { ' ' }
     <RaisedButton
       className={ styles.counterButton }
       primary
-      onClick={ onDecrement }
+      onClick={ decrementWithStep }
       icon={ <RemoveCircle /> } />
     <div className={ styles.stepInput }>
       <StepSetter />
     </div>
   </div>
+}
 
 Counter.propTypes = {
   count: PropTypes.number.isRequired,
@@ -44,13 +51,11 @@ Counter.propTypes = {
 export default connect(
   state => ({ count: getCount(state), step: getStep(state) }),
   dispatch => ({
-    onIncrement: event => {
-      event.preventDefault()
-      dispatch(incrementThunk())
+    onIncrement: step => {
+      dispatch(increment(step))
     },
-    onDecrement: event => {
-      event.preventDefault()
-      dispatch(decrementThunk())
+    onDecrement: step => {
+      dispatch(decrement(step))
     },
   })
 )(Counter)
